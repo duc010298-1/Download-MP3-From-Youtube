3d8f8ba2d91ef326a09e9eb7936ad30a55547197
Đỗ Trung Đức <duc010298@gmail.com>
2018-07-01 10:47:17 +0700

    add GUI

---
+++ Get MP3 Youtube/src/utils/MyUtils.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+/**
+ *
+ * @author Đỗ Trung Đức
+ */
+public class MyUtils {
+    
+    public String getCurrentWorkingDir() {
+        return System.getProperty("user.dir");
+    }
+    
+    
+    
+}
